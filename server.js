b47c6477aa04ca9cbc5f796f04d60890972ab28c
Nathaniel Read <nathaniel@northfolk.co.uk>
2019-03-02 18:03:36 +0000

    Added route and views for auth

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var passport    = require('passport');
 var session     = require('express-session');
 var bodyParser  = require('body-parser');
 var config      = require('config');
+var exphbs = require('express-handlebars');
 
 // bodyparser 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -17,22 +18,27 @@ app.use(passport.session());
 
 //Models
 var models = require("./app/models");
- 
+
 //Sync Database
 models.sequelize.sync().then(function() {
- 
     console.log('Nice! Database looks fine')
- 
 }).catch(function(err) {
- 
     console.log(err, "Something went wrong with the Database Update!")
- 
 });
 
-// Pages
-app.get('/', function(req,res) {
-    res.send('Hello');
-});
+//Routes
+var authRoute = require('./app/routes/auth.js')(app, passport);
+
+//load passport strategies
+require('./app/config/passport/passport.js')(passport, models.user);
+
+//For Handlebars
+app.set('views', './app/views')
+app.engine('hbs', exphbs({
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
 
 app.listen(5000,function(error) {
     if(!error)
