e99970be63c967937d16b92854238754d48044d2
Nathaniel Read <nathaniel@northfolk.co.uk>
2019-04-07 22:39:18 +0100

    Dashboard implementation

--- app/controllers/timeslotcontroller.js
+++ app/controllers/timeslotcontroller.js
@@ -1,3 +1,4 @@
+var moment  = require('moment');
 var exports = module.exports = {}
 
 exports.dashboard = function(req, res, models) {
@@ -6,13 +7,35 @@ exports.dashboard = function(req, res, models) {
     include: [{
       model: models.timeType,
       duplicating: false,
+      include: [
+        { model: models.timeTypeGroup }
+      ]
     },
-  ]
+  ],
   }).then(posts => {
+    var allEvents = [];
+    for(var i = 0; i < posts.length; i++)
+    {
+      // make data nice before sending it to the frontend
+      var durationCalc = moment.duration(moment(posts[i].endTime).diff(moment(posts[i].startTime))).asHours();
+      var thisEvent = { 
+        "typeName": posts[i].timeType.typeName,
+        "description": posts[i].description, 
+        "startTime":posts[i].startTime, 
+        "endTime":posts[i].endTime,
+        "durationHyphenated": moment(posts[i].startTime).format('hh:mm a') + " - " + moment(posts[i].endTime).format('hh:mm a'),
+        "durationHours": durationCalc,
+        "durationHoursWording": (durationCalc == 1) ? "hour" : "hours", // nice ternary to get plural correct
+        "typeIcon": "/userImages/" + posts[i].timeType.typeIcon,
+        "typeColour": posts[i].timeType.typeColour,
+        "groupTypeName": posts[i].timeType.timeTypeGroup.groupTypeName,
+      };
+      allEvents.push(thisEvent);
+    }
     console.log(posts);
+    res.render('dashboard', {allEvents: allEvents});
   });
 
-  res.render('dashboard', {});
 }
 
 exports.getBreakdown= function(req, res, models) {
